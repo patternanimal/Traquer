9a990fe9cd8088d22b00eaeafb60b63525712faa
Ryan Olson <patternanimal@gmail.com>
2019-07-05 19:02:50 -0700

    process text field

--- Traquer/ViewController.swift
+++ Traquer/ViewController.swift
@@ -8,19 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    // MARK: properties
+    //MARK: properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Handle user input through delegate callback
+        nameTextField.delegate = self
     }
-
-    // MARK: Actions
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        
+        // Do we want to always process the text field?
+        // If so, return true, otherwise handle cases
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // Chance to handle/process the input just after
+        // resigning first repsonder from textFieldShouldReturn
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
